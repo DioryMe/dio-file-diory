2f35063634bfa6ae53b1973ee51b96da83841214
Jouni Alanen <jouni@diory.me>
2022-10-23 19:41:17 +0300

    Diograph is loaded from S3 & new diory is created to it

--- handler.ts
+++ handler.ts
@@ -6,22 +6,23 @@ export const hello = async (event: any) => {
   const client = new S3Client({ region: 'eu-west-1' })
   const objectParams = { Bucket: process.env.BUCKET_NAME, Key: 'diograph.json' }
   const diographJson = await client.send(new GetObjectCommand(objectParams))
-  console.log(diographJson)
-
-  /*
-  const diographObject = ''
+  const s3BodyString = await diographJson.Body?.transformToString()
+  if (!s3BodyString) {
+    return
+  }
+  const diographObject = JSON.parse(s3BodyString)
 
   // Create new / update
-  const diograph = new Diograph()
-  diograph.mergeDiograph(diographObject)
-
+  const diograph: any = new Diograph()
+  diograph.mergeDiograph(diographObject.diograph)
   const newDiory = diograph.createDiory({ text: `New Diory: ${Date.now()}` })
   diograph.update(newDiory.id, { text: `New name: ${Date.now()}` })
 
   // Save to S3
-  // diograph.toObject()
-*/
+  const s3ObjectPayload = diograph.toObject()
+
   return {
     statusCode: 200,
+    diograph: s3ObjectPayload,
   }
 }
