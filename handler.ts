1cc299c609bf78476b3d04dbaa7cd14f404d3212
Jouni Alanen <jouni@diory.me>
2022-10-23 21:41:34 +0300

    Read dataobject to lambda with readable stream

--- handler.ts
+++ handler.ts
@@ -1,8 +1,9 @@
 import { Diograph } from 'diograph-js'
-import { GetObjectCommand, ListObjectsCommand, S3Client } from '@aws-sdk/client-s3'
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { createWriteStream } from 'fs'
+import { exec } from 'child_process'
 
-export const hello = async (event: any) => {
-  // Retrieve diograph.json from S3
+const readDiographFromS3 = async () => {
   const client = new S3Client({ region: 'eu-west-1' })
   const objectParams = { Bucket: process.env.BUCKET_NAME, Key: 'diograph.json' }
   const diographJson = await client.send(new GetObjectCommand(objectParams))
@@ -10,7 +11,39 @@ export const hello = async (event: any) => {
   if (!s3BodyString) {
     return
   }
-  const diographObject = JSON.parse(s3BodyString)
+  return JSON.parse(s3BodyString)
+}
+
+const readDataobjectFromS3ToFile = async () => {
+  const client = new S3Client({ region: 'eu-west-1' })
+  const objectParams = { Bucket: process.env.BUCKET_NAME, Key: 'PIXNIO-53799-6177x4118.jpeg' }
+  const response: any = await client.send(new GetObjectCommand(objectParams))
+  if (!response.Body) {
+    return
+  }
+  const writeStream = createWriteStream('/tmp/PIXNIO-53799-6177x4118.jpeg')
+  return response.Body.pipe(writeStream)
+}
+
+const saveDiographToS3 = async (diograph: Diograph) => {
+  return diograph.toObject()
+}
+
+export const hello = async (event: any) => {
+  const diographObject = await readDiographFromS3()
+  await readDataobjectFromS3ToFile()
+
+  exec('ls -halt /tmp', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`)
+      return
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+  })
 
   // Create new / update
   const diograph: any = new Diograph()
@@ -18,11 +51,8 @@ export const hello = async (event: any) => {
   const newDiory = diograph.createDiory({ text: `New Diory: ${Date.now()}` })
   diograph.update(newDiory.id, { text: `New name: ${Date.now()}` })
 
-  // Save to S3
-  const s3ObjectPayload = diograph.toObject()
-
   return {
     statusCode: 200,
-    diograph: s3ObjectPayload,
+    diograph: saveDiographToS3(diograph),
   }
 }
