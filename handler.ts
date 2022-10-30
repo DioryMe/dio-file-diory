d619db3738aa0ebc9565f4447080da4590cf38c9
Jouni Alanen <jouni@diory.me>
2022-10-30 19:19:28 +0200

    Upload with Camera app works - generates diory & adds to diograph successfully

--- handler.ts
+++ handler.ts
@@ -1,24 +1,10 @@
 import { Diograph, Diory, Room, RoomClient } from 'diograph-js'
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import { createWriteStream } from 'fs'
+import { createWriteStream, mkdirSync, existsSync } from 'fs'
 import { readFile } from 'fs/promises'
-import { exec } from 'child_process'
 import { Generator, getDefaultImage } from '@diograph/file-generator'
 import { LocalClient } from '@diograph/local-client'
-
-const executeSystemCall = (systemCall: string) => {
-  exec(systemCall, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`)
-      return
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`)
-      return
-    }
-    console.log(`stdout: ${stdout}`)
-  })
-}
+import { dirname } from 'path' // 'path-browserify'
 
 const readDiographFromS3 = async (bucket: string) => {
   const client = new S3Client({ region: 'eu-west-1' })
@@ -39,11 +25,16 @@ const readDataobjectFromS3ToFile = async (bucket: string, key: string): Promise<
     throw new Error(`No response.Body when retrieving ${key}!`)
   }
   return new Promise((resolve, reject) => {
-    const writeStream = createWriteStream(`/tmp/${key}`)
+    const tempPath = `/tmp/${key}`
+    const tempDir = dirname(tempPath)
+    if (!existsSync(tempDir)) {
+      mkdirSync(tempDir, { recursive: true })
+    }
+    const writeStream = createWriteStream(tempPath)
     response.Body.pipe(writeStream)
 
     response.Body.on('end', () => {
-      resolve(`/tmp/${key}`)
+      resolve(tempPath)
     })
     response.Body.on('error', (error: Error) => {
       reject(error)
@@ -51,12 +42,8 @@ const readDataobjectFromS3ToFile = async (bucket: string, key: string): Promise<
   })
 }
 
-const saveDiographToS3 = async (diograph: Diograph) => {
-  return diograph.toObject()
-}
-
 export const hello = async (event: any, context: any) => {
-  console.log('Received event:', JSON.stringify(event, null, 2))
+  // console.log('Received event:', JSON.stringify(event, null, 2))
 
   const bucket =
     event && event.Records.length ? event.Records[0].s3.bucket.name : process.env.BUCKET_NAME
@@ -71,15 +58,18 @@ export const hello = async (event: any, context: any) => {
   roomInFocus.initiateRoom({ connections: [{ address: '/tmp', contentClient: 'local' }] })
 
   const copyContent = true
-  const diographObject = await readDiographFromS3(bucket)
+
+  // TODO: Initiate room & diograph from existing files from S3
+  // const diographObject = await readDiographFromS3(bucket)
 
   const retrievedFilePath = await readDataobjectFromS3ToFile(bucket, key)
-  // const retrievedFilePath = '/tmp/PIXNIO-53799-6177x4118.jpeg'
 
   const generator = new Generator()
   const { dioryObject, thumbnailBuffer, cid } = await generator.generateDioryFromFile(
     retrievedFilePath,
   )
+  console.log(JSON.stringify(dioryObject))
+  console.log('cid', cid)
   const dataUrl = thumbnailBuffer
     ? `data:image/jpeg;base64,${thumbnailBuffer.toString('base64')}`
     : getDefaultImage()
@@ -101,14 +91,15 @@ export const hello = async (event: any, context: any) => {
 
   await roomInFocus.saveRoom()
 
-  executeSystemCall('cat /tmp/diograph.json')
-  executeSystemCall('cat /tmp/room.json')
-
   // TODO: Save diograph.json & room.json to S3
 
-  return {
+  const returnValue = {
     statusCode: 200,
     diograph: roomInFocus.diograph.toObject(),
     room: roomInFocus.toObject(),
   }
+
+  console.log(JSON.stringify(returnValue))
+
+  return returnValue
 }
